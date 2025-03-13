10ddaabf88ba576cbacd6f4c993c167fc7258350
WW <88537114+wanderwais@users.noreply.github.com>
2025-03-14 02:38:52 +0500

    Update Function.py

--- Function.py
+++ Function.py
@@ -1,1 +1,9 @@
+list = input("введите список, значения вводите через запятую, без пробелов: ").split(',')
 
+for i in list:
+  try:
+    if int(i) == float(i):
+      if list.count(i) % 2:
+        print(i)
+  except:
+    pass
