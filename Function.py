a3ce4aef4cd9d568a356650f23f2f9b0baa43f37
WW <88537114+wanderwais@users.noreply.github.com>
2025-03-14 02:46:19 +0500

    Update Function.py

--- Function.py
+++ Function.py
@@ -1,9 +1,10 @@
 list = input("введите список, значения вводите через запятую, без пробелов: ").split(',')
-
+numbers = []
 for i in list:
   try:
     if int(i) == float(i):
-      if list.count(i) % 2:
-        print(i)
+      if list.count(i) % 2 and !(i in list):
+        list.append(i)
   except:
     pass
+print(numbers)
